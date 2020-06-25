4cd25ebefbb8bbc13b24a7a828ca565706002ca4
Gary Mann <g.mann@sydney.edu.au>
2020-06-25 13:02:54 +1000

    Version 0.0.4 Release

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.planit.python","l":"PLANitJ2Py"}]
--- src/main/java/org/planit/python/PLANitJ2Py.java
+++ src/main/java/org/planit/python/PLANitJ2Py.java
@@ -1,16 +1,15 @@
 package org.planit.python;
-import java.util.logging.Logger;
 
+import java.util.logging.Logger;
 import org.planit.exceptions.PlanItException;
 import org.planit.io.project.PlanItSimpleProject;
 import org.planit.logging.Logging;
-
 import py4j.GatewayServer;
 
 /**
  * Point of entry for the Python interface. Note that:
  * Py4J by default uses the TCP port 25333 to communicate from Python to Java and TCP port 25334 to communicate from Java to Python.
- * If one encounters java.net.BindException: Address already in use exception --> manually choose a different port than the default one.
+ * If you encounter "java.net.BindException: Address already in use exception", you should manually choose a different port than the default one.
  * It is also possible a previous instance is still running. In that case first close the previous instance and try again. 
  * 
  * @author markr
@@ -30,7 +29,8 @@ public class PLANitJ2Py {
     
     /**
      * Bootstrap the gateway server
-     * @param args
+     * 
+     * @param args  arguments to main method
      */
     public static void main(String[] args) {
     	try {
@@ -52,7 +52,8 @@ public class PLANitJ2Py {
     /** Initialize the project. For now we by default create a "simple" project adopting the default I/O format. This method is invoked from
      * the Python side right after the gateway is opened in the Python wrapper (in def __init_java_gateway(self)) 
      * 
-     * @return simpleProject
+     * @param projectPath path to directory contain the project files
+     * @return the generated PlanItSimpleProject object
      */
     public PlanItSimpleProject initialiseSimpleProject(final String projectPath){
       try {
@@ -65,15 +66,17 @@ public class PLANitJ2Py {
       return (PlanItSimpleProject)this.project;      
     }
 
-    /** Due to he difficulty of creating an enum in Python (the gateway prefix to the path is inconvenient since it might change when changing
-     *  the variable name for example, we instead construct them on the Java side by providing their canonical class name solely based on the Java structure
+    /** 
+     * Due to the difficulty of creating an enum in Python (the gateway prefix to the path is inconvenient since it might change when changing
+     *  the variable name for example) we instead construct them on the Java side by providing their canonical class name solely based on the Java structure
      *  This has the benefit that we can create Python enum counterparts whose values simply reflect their Java counterpart without any Py4J "name clutter"
-     *  TODO: IF it is possible to do this as elegantly on the Python side than we should aim for that instead
+     *  TODO: If it is possible to do this as elegantly on the Python side than we should aim for that instead.
      *  
      * @param enumCanonicalName         the package and class name of the enum
-     * @param EnumEntryName             the entry of the enum that we try to instantiate
-     * @throws ClassNotFoundException 
-     * @throws PlanItException 
+     * @param EnumEntryName                 the entry of the enum that we try to instantiate
+     * @throws ClassNotFoundException    thrown if the class specified by the enumeration cannot be found
+     * @throws PlanItException                  thrown if the generated class is not an enumeration
+     * @return the enumeration value
      */
     @SuppressWarnings({"unchecked", "rawtypes"})
     public Enum createEnum(String enumCanonicalName, String EnumEntryName) throws ClassNotFoundException, PlanItException {
